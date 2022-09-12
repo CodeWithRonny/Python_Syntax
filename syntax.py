4ad1bd484ded20c2779065965080f72d8694d483
LoopBreaks <113218220+LoopBreaks@users.noreply.github.com>
2022-09-12 10:09:24 +0530

    Add files via upload

---
+++ syntax.py
@@ -0,0 +1,17 @@
+# print("Hello world!")
+# print("Hello line 2")
+
+# x = 8 # this is an comment
+# y = "String" 
+# print(x)
+
+if True:
+	print("Walla i'm printed")
+	print("Walla i'm printed")
+
+if 5>8:
+	print("HI")
+
+
+
+
